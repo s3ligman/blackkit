b78692d6cfc59b942466ba63e816a309d9651ad4
s3ligman <82592432+s3ligman@users.noreply.github.com>
2021-04-18 17:35:06 +0200

    Add files via upload

--- assets/script.js
+++ assets/script.js
@@ -14,7 +14,54 @@ function changeSide() {
 
 }
 
+
+
 // set initial side
 changeSide();
 
 radioGroup.addEventListener( 'change', changeSide );
+
+
+var front = document.getElementById("front");
+var back = document.getElementById("back");
+var left = document.getElementById("left");
+var right = document.getElementById("right");
+var active = document.querySelector('.active');
+
+
+function activateFront() {
+  front.classList.toggle( "active" );
+  back.classList.remove( "active" );
+  left.classList.remove( "active" );
+  right.classList.remove( "active" );
+
+
+}
+
+function activateBack() {
+  back.classList.toggle( "active" );
+  front.classList.remove( "active" );
+  left.classList.remove( "active" );
+  right.classList.remove( "active" );
+
+
+}
+
+function activateRight() {
+
+  right.classList.toggle( "active" );
+  front.classList.remove( "active" );
+  left.classList.remove( "active" );
+  back.classList.remove( "active" );
+
+
+}
+
+function activateLeft() {
+  left.classList.toggle( "active" );
+  front.classList.remove( "active" );
+  right.classList.remove( "active" );
+  back.classList.remove( "active" );
+
+
+}
